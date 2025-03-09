f0f31868910cb98f7b289e690d3f81b83738813c
Delorianpro <delorianpro@gmail.com>
2025-03-09 15:19:22 -0300

    Remove tag google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { title_font, subtitle_font } from "@/functions/fonts";
-import Script from "next/script";
 
 
 export const metadata: Metadata = {
@@ -16,21 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'AW-16762428286');
-            `,
-          }}
-        />
-      </head>
       <body className={`${title_font.variable} ${subtitle_font.variable}`}>
         {children}
       </body>
